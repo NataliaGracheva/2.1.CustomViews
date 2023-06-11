02d4469df8fe6b5bc4c2f20db8cf52b004e7250d
n.gracheva <gracheva_22_09@mail.ru>
2023-06-11 19:58:35 +0500

    animations+animator

--- app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
@@ -1,6 +1,8 @@
 package ru.netology.nmedia.ui
 
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.R
 
@@ -8,11 +10,27 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        findViewById<StatsView>(R.id.stats).data = listOf(
+        val view = findViewById<StatsView>(R.id.stats)
+        view.data = listOf(
             10F,
             20F,
             30F,
             40F,
         )
+
+//        1.
+//        view.startAnimation(
+//            AnimationUtils.loadAnimation(this, R.anim.animation)
+//        )
+
+//        2.
+        view.animate()
+            .rotation(360F)
+            .scaleX(1.2F)
+            .scaleY(1.2F)
+            .setInterpolator(LinearInterpolator())
+            .setStartDelay(500)
+            .setDuration(500)
+            .start()
     }
 }
